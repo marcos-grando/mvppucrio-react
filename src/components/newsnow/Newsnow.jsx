41bee435f3e8dfec436e97d4685fa9c55b04ef71
marcos-grando <168216213+marcos-grando@users.noreply.github.com>
2025-04-17 13:09:44 -0300

    import Link

--- src/components/newsnow/Newsnow.jsx
+++ src/components/newsnow/Newsnow.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import Carouselnews from "./Carouselnews";
 import Allnews from "./Allnews";
 import Title from "../Title";
